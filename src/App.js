12c0d50450e7f8bd7bb8f447bbb83912eb172c9b
Kossie Coder <kossie.coder@gmail.com>
2021-09-28 15:51:03 +1000

    07

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { useState } from 'react';
+
 function App() {
-  const printHello = () => {
-    console.log('hello')
+  const [number, setNumber] = useState(1);
+  const [number1, setNumber1] = useState(1);
+  const double = () => {
+    const doubledNumber = number * 2;
+    setNumber(doubledNumber);
+    setNumber(doubledNumber * 2);
+    setNumber1(number1 * 2);
+    console.log(doubledNumber);
   };
 
   return (
     <>
-      <button onClick={printHello}>Submit</button>
+      <div>{number}</div>
+      <div>{number1}</div>
+      <button onClick={double}>
+        Submit
+      </button>
     </>
   );
 }
