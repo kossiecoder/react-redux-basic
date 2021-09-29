5de54e7def6815a39c87e1ee50e319c00222b98d
Kossie Coder <kossie.coder@gmail.com>
2021-09-29 12:52:55 +1000

    08

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import { useState } from 'react';
 
 function App() {
+  console.log('render');
   const [number, setNumber] = useState(1);
-  const [number1, setNumber1] = useState(1);
   const double = () => {
-    const doubledNumber = number * 2;
-    setNumber(doubledNumber);
-    setNumber(doubledNumber * 2);
-    setNumber1(number1 * 2);
-    console.log(doubledNumber);
+    // const doubledNumber = number * 2;
+    setNumber((prevState) => prevState * 2);
+    setNumber((prevState) => prevState * 2);
   };
 
   return (
     <>
       <div>{number}</div>
-      <div>{number1}</div>
       <button onClick={double}>
         Submit
       </button>
