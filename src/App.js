02d0dcc0ab193a5e3362ed5c9a6614e86324d459
Kossie Coder <kossie.coder@gmail.com>
2021-09-30 14:35:09 +1000

    18

--- src/App.js
+++ src/App.js
@@ -1,73 +1,36 @@
-import { useState } from 'react';
-import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from 'react-router-dom';
+import BlogForm from './components/BlogForm';
 
 function App() {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
-  const onSubmit = () => {
-    axios.post('http://localhost:3001/posts', {
-      title,
-      body
-    })
-  };
-
   return (
     <Router>
-      <nav class="navbar navbar-dark bg-dark">
-        <div class="container">
-          <Link class="navbar-brand" to="/">Home</Link>
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <Link class="nav-link active" aria-current="page" to="/blogs">
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container">
+          <Link className="navbar-brand" to="/">Home</Link>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/blogs">
                 Blogs
               </Link>
             </li>
           </ul>
         </div>
       </nav>
-      <Switch>
-        <Route path="/" exact>
-          Home Page
-        </Route>
-        <Route path="/blogs">
-          <div className="container">
-            <h1>Create a blog post</h1>
-            <div className="mb-3">
-              <label className="form-label">Title</label>
-              <input 
-                className="form-control" 
-                value={title}
-                onChange={(event) => {
-                  setTitle(event.target.value);
-                }}
-              />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Body</label>
-              <textarea 
-                className="form-control" 
-                value={body}
-                onChange={(event) => {
-                  setBody(event.target.value);
-                }}
-                rows="20"
-              />
-            </div>
-            <button 
-              className="btn btn-primary"
-              onClick={onSubmit}
-            >
-              Post
-            </button>
-          </div>
-        </Route>
-      </Switch>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact>
+            Home Page
+          </Route>
+          <Route path="/blogs">
+            <BlogForm />
+          </Route>
+        </Switch>
+      </div>
     </Router>
     
   );
---
+++ src/components/BlogForm.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const BlogForm = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const onSubmit = () => {
+    axios.post('http://localhost:3001/posts', {
+      title,
+      body
+    })
+  };
+
+  return (
+    <div>
+      <h1>Create a blog post</h1>
+      <div className="mb-3">
+        <label className="form-label">Title</label>
+        <input 
+          className="form-control" 
+          value={title}
+          onChange={(event) => {
+            setTitle(event.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label className="form-label">Body</label>
+        <textarea 
+          className="form-control" 
+          value={body}
+          onChange={(event) => {
+            setBody(event.target.value);
+          }}
+          rows="20"
+        />
+      </div>
+      <button 
+        className="btn btn-primary"
+        onClick={onSubmit}
+      >
+        Post
+      </button>
+    </div>
+  );
+};
+
+export default BlogForm;
