39ecabc2338cf7ddf8a401331041b50a18a81df5
Kossie Coder <kossie.coder@gmail.com>
2021-10-01 12:43:43 +1000

    22

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
@@ -7,9 +7,15 @@ const NavBar = () => {
           <Link className="navbar-brand" to="/">Home</Link>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/blogs">
+              <NavLink 
+                exact
+                activeClassName="active"
+                className="nav-link" 
+                aria-current="page" 
+                to="/blogs"
+              >
                 Blogs
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </div>
