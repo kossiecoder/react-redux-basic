22bc42139aea9358e8b808b314c7f07fb8016903
Kossie Coder <kossie.coder@gmail.com>
2021-10-12 01:16:22 +1000

    23

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -1,4 +1,19 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
 const ListPage = () => {
+  const [posts, setPosts] = useState([]);
+
+  const getPosts = () => {
+    axios.get('http://localhost:3001/posts').then((res) => {
+      setPosts(res.data);
+    })
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>List Page</div>
   );
