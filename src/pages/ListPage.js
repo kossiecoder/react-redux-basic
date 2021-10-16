1f369df33cc082db5dd95fc196e5f8818333193d
Kossie Coder <kossie.coder@gmail.com>
2021-10-17 06:22:42 +1000

    30

--- src/pages/ListPage.js
+++ src/pages/ListPage.js
@@ -34,7 +34,19 @@ const ListPage = () => {
             key={post.id} 
             title={post.title} 
             onClick={() => history.push('/blogs/edit')} 
-          />
+          >
+            <div>
+              <button 
+                className="btn btn-danger btn-sm"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log('delete blog')
+                }}
+              >
+                Delete
+              </button>
+            </div>
+          </Card>
         );
       })}
     </div>
