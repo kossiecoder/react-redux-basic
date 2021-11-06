6211e89bb8b39800dd42f5707f59763f14967bcf
Kossie Coder <kossie.coder@gmail.com>
2021-11-06 14:07:44 +1000

    35

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,34 +1,9 @@
 import { useParams } from "react-router";
-import axios from 'axios';
-import { useEffect, useState } from "react";
-import LoadingSpinner from "../components/LoadingSpinner";
 
 const ShowPage = () => {
   const { id } = useParams();
-  const [post, setPost] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  const getPost = (id) => {
-    axios.get(`http://localhost:3001/posts/${id}`).then((res) => {
-      setPost(res.data)
-      setLoading(false);
-    })
-  };
-
-  useEffect(() => {
-    getPost(id)
-  }, [])
-
-  if (loading) {
-    return <LoadingSpinner />
-  }
-
-  return (
-    <div>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
-    </div>
-  );
+  console.log(id);
+  return <div>Show Page</div>;
 };
 
 export default ShowPage;
