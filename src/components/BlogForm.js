82b810bdd186dc8a155319bfcd0fbe69296e09e6
Kossie Coder <kossie.coder@gmail.com>
2022-03-04 09:45:01 +1000

    45

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -11,6 +11,8 @@ const BlogForm = ({ editing }) => {
   const [originalTitle, setOriginalTitle] = useState('');
   const [body, setBody] = useState('');
   const [originalBody, setOriginalBody] = useState('');
+  const [publish, setPublish] = useState(false);
+  const [originalPublish, setOriginalPublish] = useState(false);
 
   useEffect(() => {
     if (editing) {
@@ -19,13 +21,16 @@ const BlogForm = ({ editing }) => {
         setOriginalTitle(res.data.title);
         setBody(res.data.body);
         setOriginalBody(res.data.body);
+        setPublish(res.data.publish);
+        setOriginalPublish(res.data.publish);
       })
     }
   }, [id, editing]);
 
   const isEdited = () => {
-    console.log('a')
-    return title !== originalTitle || body !== originalBody;
+    return title !== originalTitle 
+      || body !== originalBody
+      || publish !== originalPublish;
   };
 
   const goBack = () => {
@@ -41,6 +46,7 @@ const BlogForm = ({ editing }) => {
       axios.patch(`http://localhost:3001/posts/${id}`, {
         title,
         body,
+        publish
       }).then(res => {
         console.log(res);
         history.push(`/blogs/${id}`)
@@ -49,11 +55,17 @@ const BlogForm = ({ editing }) => {
       axios.post('http://localhost:3001/posts', {
         title,
         body,
+        publish,
         createdAt: Date.now()
       }).then(() => {
         history.push('/blogs');
       })
     }
+  };
+
+  const onChangePublish = (e) => {
+    console.log(e.target.checked)
+    setPublish(e.target.checked);
   };
 
   return (
@@ -80,6 +92,18 @@ const BlogForm = ({ editing }) => {
           rows="10"
         />
       </div>
+      <div className="form-check mb-3">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            checked={publish}
+            onChange={onChangePublish}
+          />
+          <label className="form-check-label">
+            Publish
+          </label>
+      </div>
+
       <button 
         className="btn btn-primary"
         onClick={onSubmit}
