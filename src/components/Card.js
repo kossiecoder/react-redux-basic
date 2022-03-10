065f7cace4e3d39a97d7cc1b81ee0a331386f1bc
Kossie Coder <kossie.coder@gmail.com>
2022-03-11 09:41:11 +1000

    49

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,11 +6,9 @@ const Card = ({ title, onClick, children }) => {
       className="card mb-3 cursor-pointer"
       onClick={onClick}
     >
-      <div className="card-body">
-        <div className="d-flex justify-content-between">
-          <div>{title}</div>
-          {children && <div>{children}</div>}
-        </div> 
+      <div className="card-body py-2 d-flex align-items-center">
+        <div className="flex-grow-1">{title}</div>
+        {children && <div>{children}</div>}
       </div>
     </div>
   );
