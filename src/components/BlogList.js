8503107c0fe0598e99bdad2048bd36335a27d08e
Kossie Coder <kossie.coder@gmail.com>
2022-03-14 09:15:34 +1000

    51

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,6 +4,7 @@ import Card from '../components/Card';
 import { useHistory } from 'react-router';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { bool } from 'prop-types';
+import Pagination from './Pagination';
 
 const BlogList = ({ isAdmin }) => {
   const history = useHistory();
@@ -38,27 +39,35 @@ const BlogList = ({ isAdmin }) => {
   if (posts.length === 0) {
     return (<div>No blog posts found</div>)
   }
+  const renderBlogList = () => {
+    return posts.filter(post => {
+      return isAdmin || post.publish
+    }).map(post => {
+      return (
+        <Card 
+          key={post.id} 
+          title={post.title} 
+          onClick={() => history.push(`/blogs/${post.id}`)} 
+        >
+          {isAdmin ? (<div>
+            <button 
+              className="btn btn-danger btn-sm"
+              onClick={(e) => deleteBlog(e, post.id)}
+            >
+              Delete
+            </button>
+          </div>) : null}
+        </Card>
+      );
+    })
+  }
 
-  return posts.filter(post => {
-    return isAdmin || post.publish
-  }).map(post => {
-    return (
-      <Card 
-        key={post.id} 
-        title={post.title} 
-        onClick={() => history.push(`/blogs/${post.id}`)} 
-      >
-        {isAdmin ? (<div>
-          <button 
-            className="btn btn-danger btn-sm"
-            onClick={(e) => deleteBlog(e, post.id)}
-          >
-            Delete
-          </button>
-        </div>) : null}
-      </Card>
-    );
-  })
+  return (
+    <div>
+      {renderBlogList()}
+      <Pagination />
+    </div>
+  )
 };
 
 BlogList.propTypes = {
