aad696b1fb69aaa784d3122b5c11f9f247f886c3
Kossie Coder <kossie.coder@gmail.com>
2022-03-22 09:54:33 +1000

    53

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -12,7 +12,14 @@ const BlogList = ({ isAdmin }) => {
   const [loading, setLoading] = useState(true);
 
   const getPosts = (page = 1) => {
-    axios.get(`http://localhost:3001/posts?_page=${page}&_limit=5&_sort=id&_order=desc`).then((res) => {
+    axios.get(`http://localhost:3001/posts`, {
+      params: {
+        _page: page,
+        _limit: 5,
+        _sort: 'id',
+        _order: 'desc',
+      }
+    }).then((res) => {
       setPosts(res.data);
       setLoading(false);
     })
